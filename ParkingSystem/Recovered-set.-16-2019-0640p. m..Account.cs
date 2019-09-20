05ea50287bc472501aaf0c9dd43000cc94b5f5c9
Sebastián <sebapoli00@hotmail.com>
2019-09-20 20:47:33 -0300

    comienzo del tdd de account system etc, diseño de la interfaz

---
+++ ParkingSystem/Recovered-set.-16-2019-0640p. m..Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystem
+{
+    public class Account
+    {
+        public int balance { get; set; }
+
+        public Mobile mobile { get; set; }
+
+        public void addBalance(int newBalance)
+        {
+            balance += newBalance;
+        }
+
+    }
+}
